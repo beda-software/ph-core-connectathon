355ffaf68c15a58ac8d515a428194dc8b8a0083e
Ilya Beda <ir4y.ix@gmail.com>
2025-06-15 12:33:10 +0400

    Cleanup import

--- src/containers/MedicationsUberList/index.tsx
+++ src/containers/MedicationsUberList/index.tsx
@@ -2,7 +2,7 @@ import { PlusOutlined } from '@ant-design/icons';
 import { t, Trans } from '@lingui/macro';
 import { Medication } from 'fhir/r4b';
 
-import { questionnaireAction, navigationAction, ResourceListPage } from '@beda.software/emr/components';
+import { questionnaireAction, ResourceListPage } from '@beda.software/emr/components';
 
 export function MedicationsUberList() {
     return (
