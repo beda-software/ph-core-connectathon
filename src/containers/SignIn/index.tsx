3892ccabbce9ca0c41d5c82f84bcbb42e6df32d7
Ilya Beda <ir4y.ix@gmail.com>
2025-07-24 13:09:29 +1000

    Update sign up page

--- src/containers/SignIn/index.tsx
+++ src/containers/SignIn/index.tsx
@@ -25,37 +25,17 @@ export function SignIn(props: SignInProps) {
         <S.Container>
             <S.Form>
                 <S.Header>
-                    <S.Text>Welcome to Beda EMR</S.Text>
+                    <S.Text>Welcome to PH Core EMR</S.Text>
                 </S.Header>
                 <S.Message>
-                    <b>On the next page, please, use one of the following credentials</b>
-                    <S.CredentialsWrapper>
-                        <S.CredentialsBlock>
-                            <S.CredentialLabel>Username</S.CredentialLabel>
-                            <S.CredentialsList>
-                                <div>
-                                    <Tooltip title="As an admin, you have full access to settings and data">
-                                        <S.CredentialName>admin</S.CredentialName>
-                                    </Tooltip>
-                                </div>
-                            </S.CredentialsList>
-                        </S.CredentialsBlock>
-                        <S.CredentialsBlock>
-                            <S.CredentialLabel>Password</S.CredentialLabel>
-                            <S.CredentialsList>
-                                <span>
-                                    password
-                                </span>
-                            </S.CredentialsList>
-                        </S.CredentialsBlock>
-                    </S.CredentialsWrapper>
+                    <b> It is a customized version of Beda EMR that fully support PH Core FHIR IG </b>
                 </S.Message>
                 <Button
                     type="primary"
                     onClick={() => authorize({ nextUrl: props.originPathName })}
                     size="large"
                 >
-                    Log in
+                    Log in with aidbox.fhirlab.net
                 </Button>
             </S.Form>
             <AppFooter type="light" />
